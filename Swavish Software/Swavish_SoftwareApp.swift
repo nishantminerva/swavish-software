4aba872b7b2a15cc567f9f8e1f2bd580e655933b
Nishant Minerva <nishantcafe@gmail.com>
2023-09-24 21:46:25 +0530

    Initial Commit

---
+++ Swavish Software/Swavish_SoftwareApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swavish_SoftwareApp.swift
+//  Swavish Software
+//
+//  Created by Nishant Minerva on 24/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Swavish_SoftwareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
